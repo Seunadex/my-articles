8e239fc8f76da3216ed827238b84c5bb89bce19c
Adekunle Seun <Seunadex@users.noreply.github.com>
2018-01-01 17:30:11 +0100

    add authentication (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
-      resources :articles
-    end
-  end
+  mount_devise_token_auth_for 'User', at: 'auth'
+  resources :articles
 end
